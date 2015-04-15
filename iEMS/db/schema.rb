fa1d6688bdc0716b796c47736a816eb878b8f648
batakerboy <philiparon.bulleros@uap.asia>
2015-04-15 18:36:36 +0800

    fixed merge conflicts

--- iEMS/db/schema.rb
+++ iEMS/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150415070257) do
-
 
   create_table "job_levels", force: :cascade do |t|
     t.string   "rank"
